1810176dc33391a804a4dace5faca5f1d9b55410
Chick3n <chick3n@gmail.com>
2021-01-03 19:14:05 -0500

    ef migrations based on changes

--- src/Infrastructure/Persistence/Migrations/20210104000856_Initv0.1.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20210104000856_Initv0.1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SWA.Infrastructure.Persistence;
 
 namespace SWA.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210104000856_Initv0.1")]
+    partial class Initv01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Persistence/Migrations/20210104000856_Initv0.1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SWA.Infrastructure.Persistence.Migrations
+{
+    public partial class Initv01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "SiteCollections");
+
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "DiscoveryObjects",
+                newName: "RelativePath");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "SiteCollections",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "SiteCollections",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "SiteCollections",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "DiscoveryObjects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "DiscoveryObjects",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "SiteCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "SiteCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "SiteCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "DiscoveryObjects");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "DiscoveryObjects");
+
+            migrationBuilder.RenameColumn(
+                name: "RelativePath",
+                table: "DiscoveryObjects",
+                newName: "Url");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "SiteCollections",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "getdate()");
+        }
+    }
+}
