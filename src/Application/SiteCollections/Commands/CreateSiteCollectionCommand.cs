6ceb709ac43c6cb0bee7239c1669d563f5f68825
Chick3n <chick3n@gmail.com>
2021-01-03 20:08:09 -0500

    add validation behaviour to mediatr pipeline add a validator for SiteCollection command

---
+++ src/Application/SiteCollections/Commands/CreateSiteCollectionCommand.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using SWA.Application.Common.Interfaces;
+using SWA.Domain.Entities;
+using SWA.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SWA.Application.SiteCollections.Commands
+{
+    public class CreateSiteCollectionCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public SharePointType SharepointType { get; set; }
+    }
+
+    public class CreateSiteCollectionCommandHandler : IRequestHandler<CreateSiteCollectionCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateSiteCollectionCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateSiteCollectionCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new SiteCollection
+            {
+                Name = request.Name,
+                Url = request.Url,
+                SharepointType = request.SharepointType,
+                Created = DateTime.UtcNow,
+                Enabled = true
+            };
+
+            _context.SiteCollections.Add(entity);
+
+            return entity.Id;
+        }
+    }
+}
---
+++ src/Application/SiteCollections/Commands/CreateSiteCollectionCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWA.Application.SiteCollections.Commands
+{
+    public class CreateSiteCollectionCommandValidator : AbstractValidator<CreateSiteCollectionCommand>
+    {
+        public CreateSiteCollectionCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+
+            RuleFor(x => x.Url)
+                .NotEmpty()
+                .Must(x => Uri.TryCreate(x, UriKind.Absolute, out _));
+        }
+    }
+}
